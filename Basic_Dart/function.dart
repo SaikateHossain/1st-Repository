206cf36a620f85442ebaf9c4254ef80156c703b0
Saikate Hossain <kazisaikat11@gmail.com>
2024-06-20 23:59:36 +0600

    I am testing

---
+++ Basic_Dart/function.dart
@@ -0,0 +1,30 @@
+
+void main() {
+
+  print(subtract(10, 50));
+
+  print(name());
+
+  sum(40, 120);
+
+
+}
+
+
+sum(int num1, int num2){
+
+  print(num1 + num2);
+
+}
+
+int age (){
+  return 20;
+}
+
+int subtract (int x, int y){
+  return x - y;
+}
+
+String name () {
+  return 'Asif Taj';
+}
