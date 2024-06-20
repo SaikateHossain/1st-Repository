a1649d35fe09d38716dd60f5bac5d78e48f83b34
Saikate Hossain <kazisaikat11@gmail.com>
2024-06-21 00:53:16 +0600

    I am testing

---
+++ Basic_Dart/if_else.dart
@@ -0,0 +1,41 @@
+
+main() {
+
+  int age = 27;
+
+if (age >= 21){
+
+  print('Man');
+
+} else if (age >= 11){
+  print('Boy');
+} else {
+  print('Baby boy');
+}
+
+String userName = 'Sunny';
+
+if (userName == 'Sunny' && age > 20 ){
+  print('Username is $userName');
+}
+
+if (userName == 'Samim'|| userName == 'Sunny'){
+  print('Welcome home');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
